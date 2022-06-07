5c788209bf53efd3a712e2ae6fd0b47609042e82
aprian1337 <dwikyapriyan@gmail.com>
2022-06-08 02:40:23 +0700

    fix: sensitive case

--- business/coins/usecase.go
+++ business/coins/usecase.go
@@ -4,6 +4,7 @@ import (
 	businesses "aprian1337/thukul-service/business"
 	"aprian1337/thukul-service/business/coinmarket"
 	"context"
+	"strings"
 	"time"
 )
 
@@ -22,18 +23,19 @@ func NewCoinUsecase(repo Repository, coinMarketRepo coinmarket.Repository, time 
 }
 
 func (uc *CoinUsecase) GetBySymbol(ctx context.Context, symbol string) (Domain, error) {
+	symbol = strings.ToUpper(symbol)
 	data, count, err := uc.Repo.CoinsGetSymbol(ctx, symbol)
 	if err != nil {
 		return Domain{}, err
 	}
 	if count == 0 {
-		symbol, errApi := uc.CoinMarketRepo.GetBySymbol(ctx, symbol)
+		symbolData, errApi := uc.CoinMarketRepo.GetBySymbol(ctx, symbol)
 		if errApi != nil {
 			return Domain{}, businesses.ErrNotFound
 		}
 		domain := Domain{
-			Symbol: symbol.Symbol,
-			Name:   symbol.Name,
+			Symbol: symbolData.Symbol,
+			Name:   symbolData.Name,
 		}
 		createSymbol, err := uc.Repo.CoinsCreateSymbol(ctx, domain)
 		if err != nil {
@@ -45,7 +47,7 @@ func (uc *CoinUsecase) GetBySymbol(ctx context.Context, symbol string) (Domain, 
 	return data, nil
 }
 
-func (uc *CoinUsecase) GetAllSymbol(ctx context.Context) ([]Domain, error){
+func (uc *CoinUsecase) GetAllSymbol(ctx context.Context) ([]Domain, error) {
 	data, err := uc.Repo.GetAllSymbol(ctx)
 	if err != nil {
 		return []Domain{}, err
